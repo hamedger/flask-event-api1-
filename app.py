cee4c4ea37438c2fc994cde4af0b942c8e492198
hamedger <promitsinc@gmail.com>
2025-07-22 09:31:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def get_cities():
 
 @app.route('/', methods=['GET'])
 def get_events():
-    city = request.args.get('city')
+     city = request.args.get('city', 'New York')  # Default to New York if city not provided
     country = request.args.get('country', 'US')  # Defaults to US
     sort_by = request.args.get('sort')  # e.g., 'date', 'attendance', 'source'
     
